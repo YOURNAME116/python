b40389b53659bb874ff54c6ecbca269334077992
Utsab Adhikari <100577045+YOURNAME116@users.noreply.github.com>
2023-04-05 21:39:05 +0545

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,10 @@
+class father:
+    def land (self):
+        print("got 10 acers of land")
+class son(father):
+    def money(self):
+        print("got 10 lakhs of money")
+    
+obj=son()
+obj.land()
+obj.money()
