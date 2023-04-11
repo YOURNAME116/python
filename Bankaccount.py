530c9a800dd56fc42f9d529b27e03c3c9f5bcb18
Utsab Adhikari <100577045+YOURNAME116@users.noreply.github.com>
2023-04-11 18:45:03 +0545

    Create Bankaccount.py

---
+++ Bankaccount.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    balance=0
+    owner="utshav Adhikari"
+    def WithDrwal(self,WithdrawlAmount):
+        self.amount=WithdrawlAmount
+        if self.balance < self.amount:
+            return("you cannot withdraw the amount")
+        else:
+            self.balance=self.balance-self.amount
+            return (f"you have withdraw amount RS.{self.amount} Now, your current balace is RS.{self.balance}")
+    def Deposit(self,DepositAmount):
+        self.amount=DepositAmount
+        self.balance = self.balance + self.amount
+        return (f"You have deposited RS.{self.amount} Now, your current balance is RS.{self.balance} ")
+    def Current_balance(self):
+        return(f"Your total balance is RS.{self.balance}")
+
+if __name__ == '__main__':
+    person1=BankAccount()
+    
+    print(person1.Deposit(1000))
+    print(person1.WithDrwal(500))
+    print(person1.Deposit(39))
+    print(person1.Current_balance())
